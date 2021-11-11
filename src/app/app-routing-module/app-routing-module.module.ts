5c707fc3547ef895e069b1cc51086095567cb4a3
Cholewa.K <cholewa.k@eplan.pl>
2021-11-11 11:03:47 +0100

    ADD: Handle not found page

--- src/app/app-routing-module/app-routing-module.module.ts
+++ src/app/app-routing-module/app-routing-module.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ContactPageComponent} from "../contact-page/contact-page.component";
 import { HomePageComponent} from "../home-page/home-page.component";
+import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
 
 const routes: Routes = [
   { path: 'home', component:  HomePageComponent},
   { path: 'contact', component: ContactPageComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModuleModule } from './app-routing-module/app-routing-module.module'
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
