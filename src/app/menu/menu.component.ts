21371f73a849989088c93d3d8c2eccf504cad665
Konrad <konradxd7@gmail.com>
2024-02-03 12:50:56 +0100

    ADD: Improvment menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+export interface PageList {
+  name: string,
+  link: string,
+}
 @Component({
   selector: 'app-menu',
   standalone: true,
@@ -9,5 +13,21 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
-
+  public manuList: Array<PageList> = [
+    {
+      name: 'Strona główna',
+      link: ''
+    },
+    {
+      name: 'Usługi',
+      link: ''
+    },
+    {
+      name: 'Galleria',
+      link: ''
+    },
+    {
+      name: 'Kontakt',
+      link: ''
+    }];
 }
