6dbe844241fd375b66a9e7118b2940f7806d853a
Konrad <konradxd7@gmail.com>
2024-02-03 12:28:33 +0100

    ADD: Initial menu component

--- src/app/head/head.component.ts
+++ src/app/head/head.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {CommonModule} from "@angular/common";
+import {MenuComponent} from "../menu/menu.component";
 
 @Component({
   selector: 'app-head',
   standalone: true,
   templateUrl: './head.component.html',
   styleUrls: ['./head.component.scss'],
-  imports: [CommonModule]
+    imports: [CommonModule, MenuComponent]
 })
 export class HeadComponent implements OnInit {
 
